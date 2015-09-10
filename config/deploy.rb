101575ff1843114cecdc5c1cabd94224e6dd2973
Ammon Shepherd <ammon.shepherd@gmail.com>
2015-09-10 13:20:24 -0400

    Replace README text

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'eventually'
+set :repo_url, 'https://github.com/mossiso/eventually-test.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
